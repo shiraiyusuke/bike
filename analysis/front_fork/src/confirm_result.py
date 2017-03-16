b622261dd1ea164ba557cb72b05d877adc06e302
01001272 <yusuke_shirai@r.recruit.co.jp>
2017-03-17 08:21:50 +0900

    test

--- analysis/front_fork/src/confirm_result.py
+++ analysis/front_fork/src/confirm_result.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
-from pandas.io.parsers import read_csv
 from keras.models import model_from_json
 import matplotlib.pyplot as plt
-import bike_key_point_train
+import key_point_train
+import data_load as dl
+import cv2
 
 FTRAIN = '../data/bike_training.csv'
 FTEST = '../data/bike_test_ng.csv'
@@ -12,8 +13,8 @@ FTEST = '../data/bike_test_ng.csv'
 def drew_sample(model1, model2):
     # sampleの[1:2]を変えるとサンプルが変化
     for i in range(42):
-        sample1 = bike_key_point_train.load2d(test=True)[0][i:i+1]
-        sample2 = bike_key_point_train.load2d(test=True)[0][i:i+1]
+        sample1 = key_point_train.load2d(test=True)[0][i:i + 1]
+        sample2 = key_point_train.load2d(test=True)[0][i:i + 1]
         y_pred1 = model1.predict(sample1)[0]
         y_pred2 = model2.predict(sample2)[0]
         print y_pred2
@@ -25,95 +26,58 @@ def drew_sample(model1, model2):
         plot_sample(sample2, y_pred2, ax)
         plt.show()
 
-    """
-    sample1 = load(test=True)[0][7:8]  #[7:8]を変えるとサンプルが変化する。
-    sample2 = load2d(test=True)[0][9:10]
-    y_pred1 = model1.predict(sample1)[0]
-    y_pred2 = model2.predict(sample2)[0]
-    print y_pred2
-
-    fig = plt.figure(figsize=(6, 3))
-    ax = fig.add_subplot(1, 2, 1, xticks=[], yticks=[])
-    plot_sample(sample1, y_pred1, ax)
-    ax = fig.add_subplot(1, 2, 2, xticks=[], yticks=[])
-    plot_sample(sample2, y_pred2, ax)
-    plt.show()
-    """
-
-def data_augmentation():
-    # 画像の反転
-    X, y = load2d()
-    X_flipped = X[:, :, ::-1, :] # 左右反転
-    # X_flipped = X[:, :, :, ::-1]
-    # X_flipped = X[:, ::-1, :, :] # 上下反転
-
-    fig = plt.figure(figsize=(6, 3))
-    ax = fig.add_subplot(1, 2, 1, xticks=[], yticks=[])
-    plot_sample(X[1], y[1], ax)
-    ax = fig.add_subplot(1, 2, 2, xticks=[], yticks=[])
-    plot_sample(X_flipped[1], y[1], ax)
-    plt.show()
-
-    # keypointの反転
-    flip_indices = [
-        (0, 2), (1, 3),
-        (4, 8), (5, 9), (6, 10), (7, 11),
-        (12, 16), (13, 17), (14, 18), (15, 19),
-        (22, 24), (23, 25),
-        ]
-
-    df = read_csv(os.path.expanduser(FTRAIN))
-    for i, j in flip_indices:
-        print("{} -> {}".format(df.columns[i], df.columns[j]))
-
-
 def plot_sample(x, y, axis):
+    print y
     img = x.reshape(300, 400)
     axis.imshow(img, cmap='gray')
-    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
+    #print y[:,0::2] = y[:,0::2] * 200 + 200
+    #y[:,1::2] = y[:,1::2] * 150 + 150
+    axis.scatter(y[:,0::2] * 200 + 200, y[:,1::2] * 150 + 150, marker='x', s=10)
 
+def drew_sample(model_dict):
+    for key in model_dict:
+        # モデル読み込み
+        model_architecture = model_dict[key].split(' ')[0]
+        model_weight = model_dict[key].split(' ')[1]
+        model = model_from_json(open(model_architecture).read())
+        model.load_weights(model_weight)
 
+        # テスト用画像の読み込み
+        image_file = '/usr/local/wk/git_local/bike/tool/bike_make_tag/data/resize_random_clop_left_gray/gray_2_021420160309_l_300_400.jpg'
+        input_shape = (400, 300)
+        X = dl.image_load2d(image_file, input_shape)
 
+        # 座標予測
+        y_pred = model.predict(X)
+        # y_pred = model.predict(X_img)
+        print y_pred
+
+        # 結果描写
+        fig = plt.figure(figsize=(12, 6))
+        ax = fig.add_subplot(1, 1, 1, xticks=[], yticks=[]) # subplot(行数, 列数, 何番目のプロットか)
+        plot_sample(X, y_pred, ax)
+
+        # plot_sample(X, y_pred, ax)
+        plt.show()
+        print 'end'
 
 
 
 if __name__ == '__main__':
-    model1_architecture = '../model/bike_model1_architecture_5.json'
-    model1_weight = '../model/bike_model1_weights_5.h5'
-    # make_simple_model(model1_architecture, model1_weight)
-    # make_simple_model()
+    # 予測に利用するモデル指定
+    model_dict = {}
+    point_list = ['right_bottom']
+    # point_list = ['left_top', 'left_middle', 'left_bottom', 'right_top', 'right_middle', 'right_bottom']
 
-    conv_model1_architecture = '../model/bike_conv_model_architecture_arg_8.json'
-    conv_model1_weight = '../model/bike_conv_model_weights_arg_8.h5'
+    for point in point_list:
+        model_dict[point] = '../model/each/' + point + '/bike_conv_model3_architecture_' + point + '.json ' + '../model/each/' + point + '/bike_conv_model3_' + point + '_weights.h5'
 
-    conv_model2_architecture = '../model/bike_conv_model_architecture_arg.json'
-    conv_model2_weight = '../model/bike_conv_model_weights_arg.h5'
-
-    # make_conv_model()
-    # make_conv_model3()
-
-    # モデルの読み込み
-    model1 = model_from_json(open(conv_model1_architecture).read())
-    model1.load_weights(conv_model1_weight)
-    model2 = model_from_json(open(conv_model2_architecture).read())
-    model2.load_weights(conv_model2_weight)
-    #model2 = model_from_json(open(model2_architecture).read())
-    #model2.load_weights(model2_weight)
-
-    # ネットワーク描写
-    # plot(conv_model1, to_file='model2.png', show_shapes=True)
+    for key in model_dict:
+        if not os.path.exists(model_dict[key].split(' ')[0]):
+            print '[ERR]modelがない' + model_dict[key].split(' ')[0]
+        if not os.path.exists(model_dict[key].split(' ')[1]):
+            print '[ERR]modelがない' + model_dict[key].split(' ')[1]
 
     # 1サンプルの描画
-    drew_sample(model1, model2)
-
-    # データオーグメンテーション
-    # data_augmentation()
-
-    # X, y = load2d()
-    # X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # start = 0.03
-    # stop = 0.001
-    # nb_epoch = 5000
-    # learning_rates = np.linspace(start, stop, nb_epoch)
+    drew_sample(model_dict)
 
--- analysis/front_fork/src/key_point_train.py
+++ analysis/front_fork/src/key_point_train.py
@@ -6,29 +6,55 @@ from sklearn.utils import shuffle
 from sklearn.cross_validation import train_test_split
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation
 from keras.optimizers import SGD
 from keras.models import model_from_json
-from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dropout
-from keras.utils.visualize_util import plot
-from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import LearningRateScheduler
-
-from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
-from keras.regularizers import l2
-
-
 import matplotlib.pyplot as plt
+import data_load as dl
 
 FTRAIN = '../data/bike_training.csv'
 FTEST = '../data/bike_test_ng.csv'
 
 
+def make_simple_model():
+    X, y = dl.load()
+    print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(X.shape, X.min(), X.max()))
+    print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(y.shape, y.min(), y.max()))
+
+    model = Sequential()
+    model.add(Dense(100, input_dim=120000))
+    model.add(Activation('relu'))
+    model.add(Dense(12))
+
+    sgd = SGD(lr=0.001, momentum=0.9, nesterov=True) # Nesterov accelerated gradient[NAG]を利用
+    model.compile(loss='mean_squared_error', optimizer=sgd)
+    hist = model.fit(X, y, nb_epoch=30, validation_split=0.2) # validation_split=0.2でサンプルのうち20%をバリデーションに。
+    drew_accuracy(hist)
+
+    json_string = model.to_json()
+    open('../model/bike_model1_architecture_5.json', 'w').write(json_string)
+    model.save_weights('../model/bike_model1_weights_5.h5')
+
+    X_test, _ = dl.load(test=True)
+    y_test = model.predict(X_test)
+
+    fig = plt.figure(figsize=(6, 6))
+    fig.subplots_adjust(
+        left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+    for i in range(16):
+        axis = fig.add_subplot(4, 4, i+1, xticks=[], yticks=[])
+        print(y_test[i])
+        plot_sample(X_test[i], y_test[i], axis)
+
+    plt.show()
+
+
 def make_conv_model():
-    X, y = load2d()
+    input_shape=(300, 400)
+    X, y = dl.load2d(test=False, cols=None, shape=input_shape, backend='tensorflow')
     model2 = Sequential()
 
     # model2.add(Convolution2D(32, 3, 3, input_shape=(1, 96, 96))) # こっちだと上手くいかなかった(by theano)
@@ -52,7 +78,7 @@ def make_conv_model():
     model2.add(Activation('relu'))
     model2.add(Dense(12))
 
-    sgd = SGD(lr=0.01, momentum=0.9, nesterov=True)
+    sgd = SGD(lr=0.000001, momentum=0.9, nesterov=True)
     model2.compile(loss='mean_squared_error', optimizer=sgd)
     hist2 = model2.fit(X, y, nb_epoch=1000, validation_split=0.2)
 
@@ -62,7 +88,9 @@ def make_conv_model():
 
 
 def make_conv_model3():
-    X, y = load2d()
+    X, y = dl.load2d(test=False, cols=None, shape=(300, 400), backend='tensorflow')
+    print y
+    print X
     model3 = Sequential()
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -109,8 +137,8 @@ def make_conv_model3():
 def drew_sample(model1, model2):
     # sampleの[1:2]を変えるとサンプルが変化
     for i in range(42):
-        sample1 = load2d(test=True)[0][i:i+1]
-        sample2 = load2d(test=True)[0][i:i+1]
+        sample1 = dl.load2d(test=True)[0][i:i+1]
+        sample2 = dl.load2d(test=True)[0][i:i+1]
         y_pred1 = model1.predict(sample1)[0]
         y_pred2 = model2.predict(sample2)[0]
         print y_pred2
@@ -137,92 +165,17 @@ def drew_sample(model1, model2):
     plt.show()
     """
 
-def make_simple_model(model_architecture='', model_weight=''):
-    if model_weight == '':
-        X, y = load()
-        print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(X.shape, X.min(), X.max()))
-        print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(y.shape, y.min(), y.max()))
-
-        model = Sequential()
-        model.add(Dense(100, input_dim=120000))
-        model.add(Activation('relu'))
-        model.add(Dense(12))
-
-        sgd = SGD(lr=0.05, momentum=0.9, nesterov=True) # Nesterov accelerated gradient[NAG]を利用
-        model.compile(loss='mean_squared_error', optimizer=sgd)
-        hist = model.fit(X, y, nb_epoch=30, validation_split=0.2) # validation_split=0.2でサンプルのうち20%をバリデーションに。
-
-        json_string = model.to_json()
-        open('../model/bike_model1_architecture_5.json', 'w').write(json_string)
-        model.save_weights('../model/bike_model1_weights_5.h5')
-
-        plt.plot(hist.history['loss'], linewidth=3, label='train')
-        plt.plot(hist.history['val_loss'], linewidth=3, label='valid')
-        plt.grid()
-        plt.legend()
-        plt.xlabel('epoch')
-        plt.ylabel('loss')
-        plt.yscale('log')
-        plt.show()
 
 
-    elif model_architecture != '':
-        # モデルの読み込み
-        model = model_from_json(open(model_architecture).read())
-        model.load_weights(model_weight)
-
-    X_test, _ = load(test=True)
-    y_test = model.predict(X_test)
-
-    fig = plt.figure(figsize=(6, 6))
-    fig.subplots_adjust(
-        left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
-
-    for i in range(16):
-        axis = fig.add_subplot(4, 4, i+1, xticks=[], yticks=[])
-        print(y_test[i])
-        plot_sample(X_test[i], y_test[i], axis)
-
+def drew_accuracy(hist):
+    plt.plot(hist.history['loss'], linewidth=3, label='train')
+    plt.plot(hist.history['val_loss'], linewidth=3, label='valid')
+    plt.grid()
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.yscale('log')
     plt.show()
-
-def load(test=False, cols=None):
-    fname = FTEST if test else FTRAIN
-    df = read_csv(os.path.expanduser(fname))
-    # print(df.head())
-
-    # グレースケールのピクセル値をnumpyのarrayに変換
-    df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep =' '))
-
-    if cols:
-        df = df[list(cols) + ['Image']]
-
-    print(df.count())
-    df = df.dropna()
-
-    X = np.vstack(df['Image'].values) / 255. # 0〜1の値に変換
-    X = X.astype(np.float32)
-
-    if not test:
-        y = df[df.columns[:-1]].values # 最後のカラム[Image]以外(正解ラベル)を取得。
-        # y = df[df.columns[0:10]].values # 正解のデータ個数を変えたければ
-        y = (y - 48) / 48
-        X, y = shuffle(X, y, random_state=42) # データをシャッフル
-        y = y.astype(np.float32)
-    else:
-        y = None
-
-    return X, y
-
-
-def load2d(test=False, cols=None):
-    X, y = load(test, cols)
-    # X = X.reshape(-1, 1, 96, 96)
-    # X = X.reshape(-1, 1, 300, 400)
-    # X = X.reshape(-1, 96, 96, 1)
-    X = X.reshape(-1, 300, 400, 1)
-    return X, y
-
-
 
 
 if __name__ == '__main__':
@@ -238,7 +191,7 @@ if __name__ == '__main__':
     conv_model2_weight = '../model/bike_conv_model_weights_arg.h5'
 
     # make_conv_model()
-    # make_conv_model3()
+    make_conv_model3()
 
     # モデルの読み込み
     model1 = model_from_json(open(conv_model1_architecture).read())
@@ -247,21 +200,4 @@ if __name__ == '__main__':
     model2.load_weights(conv_model2_weight)
     #model2 = model_from_json(open(model2_architecture).read())
     #model2.load_weights(model2_weight)
-
-    # ネットワーク描写
-    # plot(conv_model1, to_file='model2.png', show_shapes=True)
-
-    # 1サンプルの描画
-    drew_sample(model1, model2)
-
-    # データオーグメンテーション
-    # data_augmentation()
-
-    # X, y = load2d()
-    # X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # start = 0.03
-    # stop = 0.001
-    # nb_epoch = 5000
-    # learning_rates = np.linspace(start, stop, nb_epoch)
 
